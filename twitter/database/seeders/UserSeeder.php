a789b3f47bbf5e053bdfe6073bfce4b055bdad18
恒川泰智 <tsunekawataichi@Hajimari-TsunekawaTaichi.local>
2023-07-14 16:29:24 +0900

    シーディングでユーザー情報を追加できるようにした

---
+++ twitter/database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 1; $i <= 10; $i++) {
+            User::create([
+                "name" => "ユーザー$i",
+                "email" => "user$i@test",
+                "password" => "password",
+            ]);
+        }
+    }
+}
